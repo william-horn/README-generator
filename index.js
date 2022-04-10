50961ada20852ae4c41454ba2dd36886fe414deb
William Horn <williamjosephhorn@gmail.com>
2022-04-10 18:37:56 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+/* --------------- */
+/* Require Modules */
+/* --------------- */
+// TODO: Include packages needed for this application
+// *DONE
+// const fs = require('fs');
+// const inquirer = require('inquirer');
+import fs from 'fs';
+import inquirer from 'inquirer';
+const generateMarkdown = require('./lib/generate-markdown');
+
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
