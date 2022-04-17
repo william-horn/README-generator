5e47b3bdbad4d51c7115785765293e670deddf22
William Horn <williamjosephhorn@gmail.com>
2022-04-17 15:17:45 -0400

    added modules and initiate user input

---
+++ README.md
@@ -0,0 +1,29 @@
+
+  # README Factory
+  ## Description
+  This is an application designed to help you generate your own README.md files with critical information while a majority of the work is done for you!
+  ## License
+  
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+This application is distributed under the [MIT](https://opensource.org/licenses/MIT) license.
+  ## Table of contents
+  - [License](#License)
+  - [Usage](#Usage)
+  - [Installation](#Installation)
+  - [Testing](#Testing)
+  - [Contributions](#Contributions)
+  ## Usage
+  Use this application to save time and writing when working on your next project. Why write the same hundreds or thousands of words out each time you want to create a readme when you only need a template? This application provides this 'readme template' to you!
+  ## Installation
+  To get started, first you need to have Node.js installed. Then, check out the video below:
+  ## Tests
+  No tests currently
+  ## Contributors
+  Contributors: 
+  William J. Horn
+  ## Questions
+  Find me on GitHub: <https://github.com/william-horn>
+  
+Reach me by email: williamjosephhorn@gmail.com
+  
--- index.js
+++ index.js
@@ -1,24 +1,116 @@
+/*
+? @document-start
+======================
+| MAIN PROGRAM LOGIC |
+==================================================================================================================================
+
+? @author:                 William J. Horn
+? @document-name:          index.js
+? @document-created:       04/15/2022
+? @document-modified:      04/15/2022
+
+==================================================================================================================================
+
+? @document-info
+==================
+| ABOUT DOCUMENT |
+==================================================================================================================================
+
+Main program logic behind application. This document is responsible for collecting, processing, and parsing user input to 
+construct a markdown-formatted string. The program will then create/append the markdown string to a README.md file
+in the root directory.
+
+==================================================================================================================================
+
+? @document-todo
+========
+| TODO |
+==================================================================================================================================
+-
+==================================================================================================================================
+*/
 
 /* --------------- */
 /* Require Modules */
 /* --------------- */
-// TODO: Include packages needed for this application
-// *DONE
-// const fs = require('fs');
-// const inquirer = require('inquirer');
-import fs from 'fs';
-import inquirer from 'inquirer';
-const generateMarkdown = require('./lib/generate-markdown');
+// todo: Include packages needed for this application
+// *--DONE
+const fs = require('fs');
+const inquirer = require('inquirer');
+const { generateMarkdown } = require('./lib/generate-markdown');
+const licenseInfo = require('./lib/license-data').licenseInfo;
 
+/* ----------------------- */
+/* Internal Program States */
+/* ----------------------- */
+// todo: Create an array of questions for user input
+// *--DONE
+const questions = [
+    // Text-input begins //
+    {
+        name: 'title',
+        message: 'Enter the title of your README:'
+    },
+    {
+        name: 'description',
+        message: 'Enter the description of your README:',
+    },
+    {
+        name: 'installation',
+        message: 'Enter installation details:'
+    },
+    {
+        name: 'usage',
+        message: 'Enter app usage details:'
+    },
+    {
+        name: 'contributions',
+        message: 'Enter contribution details:'
+    },
+    {
+        name: 'tests',
+        message: 'Enter information about tests:'
+    },
+    {
+        name: 'githubName',
+        message: 'Enter GitHub name:'
+    },
+    {
+        name: 'email',
+        message: 'Enter your email:'
+    },
+    // Text-input ends //
 
-// TODO: Create an array of questions for user input
-const questions = [];
+    // List-input begins //
+    {
+        type: 'list',
+        name: 'licenseType',
+        message: 'Choose a lisence:',
+        choices: licenseInfo,
+    },
+    // List-input ends //
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+/* ------------------------- */
+/* General-Purpose Functions */
+/* ------------------------- */
+// todo: Create a function to write README file
+// *--DONE
+const writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, err => {
+        if (err) throw err;
+        console.log('README has been generated in \'README.md\' file');
+    });
+}
 
-// TODO: Create a function to initialize app
-function init() {}
+// todo: Create a function to initialize app
+// *--DONE
+const init = () => {
+    inquirer
+        .prompt(questions) // prompt the user with readme questions
+        .then(readmeData => generateMarkdown(readmeData))
+        .then(markdownData => writeToFile('README.md', markdownData))
+}
 
 // Function call to initialize app
 init();
